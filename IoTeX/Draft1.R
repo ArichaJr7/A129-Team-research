9d14e2480b56f065f1389dec4ef0a17315f0c949
kp18aao <kp18aao@herts.ac.uk>
2024-11-20 01:47:25 +0000

    Datatypes
    I have looked at the datatypes using str() and it shows that the date datatype is chr and not a DATE datatype. I will need to investigate and convert this

--- IoTeX/Draft1.R
+++ IoTeX/Draft1.R
@@ -1,7 +1,14 @@
-library(datasets)
-iotex <- read.csv('IoTeX.csv')
-head(iotex)
-summary(iotex)
+
+library(datasets) # Initialises the 'dataset' package
+iotex <- read.csv('IoTeX.csv') # Gets the dataset from the folder and names the object 'iotex' for userbility
+head(iotex) # shows the first 6 rows
+summary(iotex) # gives a summary of the dataset
+
+str(iotex) # shows the data type of the data
+
+#The 'Date' Datatype is chr and I want to change this to a date datatype
+
+
 
 # Clear environment
 rm(list = ls()) 
