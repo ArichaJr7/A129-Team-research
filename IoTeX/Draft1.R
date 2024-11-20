1209cfe5e8f99deb937b44e853f3f97aa9d70f32
kp18aao <kp18aao@herts.ac.uk>
2024-11-20 01:30:40 +0000

    Getting Started
    Fixed problem in the previous commit
    
    the dataset is able to be read with additional packages
    
    I can see the first couple of rows using the head() and also see a summary of the dataset using the Summary()
    
    also some of the clear functions have been added to the end of the code to clear the environment and plots. This code was taught by freeCodeCamp.org on youtube

--- IoTeX/Draft1.R
+++ IoTeX/Draft1.R
@@ -1,1 +1,14 @@
 library(datasets)
+iotex <- read.csv('IoTeX.csv')
+head(iotex)
+summary(iotex)
+
+# Clear environment
+rm(list = ls()) 
+# Clear packages
+detach("package:datasets", unload = TRUE)  # For base
+# Clear plots
+dev.off()  # But only if there IS a plot
+# Clear console
+cat("\014")  # ctrl+L
+# Clear mind :)
